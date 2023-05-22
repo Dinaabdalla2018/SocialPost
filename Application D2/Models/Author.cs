6ee3a86ee58bc428e8ad9b627e46eba2926bcd15
Dina2018 <43096470+Dinaabdalla2018@users.noreply.github.com>
2023-05-22 12:51:07 +0300

    Add files via upload

---
+++ Application D2/Models/Author.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_D2.Models
+{
+    public class Author
+    {
+
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(50)]
+        public string Username { get; set; }
+        [StringLength(50)]
+        public string Password { get; set; }
+        [Column(TypeName = "image")]
+        public byte[]? photo { get; set; }
+        public int? Age { get; set; }
+
+        public virtual List<Post> posts { get; set; } = new List<Post>();
+
+    }
+}
+
---
+++ Application D2/Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_D2.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        public string? Desc { get; set; }
+
+        public virtual List<Post> posts { get; set; } = new List<Post>();
+    }
+}
